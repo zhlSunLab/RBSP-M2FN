3af223668947287cd27d6ca99d4fec76a3eabe89
zhlSunLab <93773588+zhlSunLab@users.noreply.github.com>
2024-02-02 21:03:42 +0800

    Add files via upload

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+
+class DefinedConfig(object):
+    batch_size = 16
+    splite_rate = 0.9
+    epochs = 60
+    class_nums = 1
+    cutoff_seq_len = 64   ### 70 60 no; 
+    threadhold = 0.6
+    dropout = 0.1
